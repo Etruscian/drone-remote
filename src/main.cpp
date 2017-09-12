6d94624a434952e2f9dd8880403ab0ea91543471
Etruscian <sanswart@gmail.com>
2017-09-12 15:55:17 +0200

    removed abundant comments and code

--- src/main.cpp
+++ src/main.cpp
@@ -25,56 +25,47 @@ Not SPI
 */
 
 #define PAYLOAD_SIZE 8
-#define DEBUG__
+#define TIMERCOUNTERVALUE 0x83
+// #define DEBUG__
 
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 RF24 radio(9,10);
 
-// Set input pin
-const int pin_throttle = A0;    // set A0 as input
-const int pin_roll = A1;
-const int pin_pitch = A2;
-const int pin_yaw = A3;
-const int pin_switch1 = 7;
-const int pin_switch2 = 8;
+// Set input pins
+const int pinThrottle = A0;
+const int pinRoll = A1;
+const int pinPitch = A2;
+const int pinYaw = A3;
+const int pinSwitch1 = 7;
+const int pinSwitch2 = 8;
+
 bool success;
+
 uint16_t payload[PAYLOAD_SIZE];
 uint8_t acknowledgePayload[4];
 
-const uint64_t pipe =  0x00F0F0F0F0; 
+const uint64_t address =  0x00F0F0F0F0; 
 
 void setup(void)
 {
-//  Serial.begin(115200);    //set usb speed
-//  printf_begin();         //run printf file to be able to print
-//  printf("\n\rRF24/examples/pingpair/\n\r");
-
-  //
-  // Setup and configure rf radio
-  //
-
-  radio.begin();        // initiliaze radio file "RF24.h".
+#ifdef DEBUG__
   Serial.begin(2000000);
+#endif
+
+  radio.begin();
   radio.setRetries(15,15); // 15 times 15 micro seconds retries
-
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  // radio.setPayloadSize(PAYLOAD_SIZE);  // number of bytes (total bits = bytes *8 bits)
   radio.setChannel(101);    // Radio channel expects channel code.
-
-  radio.openWritingPipe(pipe);      // use first adress in pipes
-  // radio.openReadingPipe(1,pipe);    // use second adress in pipes
-
-  radio.setPALevel(RF_PWR_LOW);
-  // radio.setAutoAck(1,true);
+  radio.openWritingaddress(address);
+  radio.setPALevel(RF_PWR_HIGH);
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
-  //
-  // Start listening
-  //
-  radio.startListening();                 // enable radio
 
-  // First, stop listening so we can talk.
+  // enable radio
+  // TODO: make separate function for enable radio
+  radio.startListening();
+
+  // Put in transmit mode
+  // TODO: rename to transmit mode
   radio.stopListening();
 
   // set up interrupt so that readings are done with 500Hz
@@ -82,30 +73,30 @@ void setup(void)
   TCCR2B = 0;
   TCCR2B |= 1 << WGM12;
   TCCR2B |= 1 << CS12;
-  TCNT2 = 0x83;
+  TCNT2 = TIMERCOUNTERVALUE;
   TIMSK2 |= 1 << OCIE2A;
- interrupts();
- Serial.println("starting");
+  interrupts();
 }
 
-// Interrupt service routine
 ISR(TIMER2_COMPA_vect){
-  TCNT2 = 0x83;
-      // Read input
+  TCNT2 = TIMERCOUNTERVALUE;
   
-  payload[0] = analogRead(pin_throttle);
-  payload[1] = analogRead(pin_roll);
-  payload[2] = analogRead(pin_pitch);
-  payload[3] = analogRead(pin_yaw);
-  payload[4] = digitalRead(pin_switch1)<<1 | (digitalRead(pin_switch2) &0x01);
+  payload[0] = analogRead(pinThrottle);
+  payload[1] = analogRead(pinRoll);
+  payload[2] = analogRead(pinPitch);
+  payload[3] = analogRead(pinYaw);
+  payload[4] = digitalRead(pinSwitch1)<<1 | (digitalRead(pinSwitch2) &0x01);
 
   success = radio.write( &payload, PAYLOAD_SIZE );
+
   if (radio.isAckPayloadAvailable()){
     success = radio.read(&acknowledgePayload,4);
 #ifdef DEBUG__
-    // Serial.println(acknowledgePayload[1] <<8 | acknowledgePayload[0]);
+    Serial.println(acknowledgePayload[1] <<8 | acknowledgePayload[0]);
 #endif
-  } else {}
+  } else {
+    
+  }
 }
 
 void loop(void)
